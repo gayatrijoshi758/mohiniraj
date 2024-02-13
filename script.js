e4f0a8d1dd58480e926b9b613d5b687c15cd442b
Gayatri Joshi <gj06102003@gmail.com>
2024-02-14 01:03:38 +0530

    added

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 
-document.addEventListener("DOMContentLoaded",function()
-{
+document.addEventListener("DOMContentLoaded", function() {
 
-    var images=document.querySelectorAll('.img-section img');
-    var currentIndex=0;
-    var interval=10000;
-
-    function changeImage(){
-
-        for(var i=0; i<images.length; i++){
-            images[i].style.display='none';
-        }
-
-        currentIndex=(currentIndex+1)%images.length;
-        images[currentIndex].style.display='block';
+    var images = document.querySelectorAll('.img-section img');
+    var currentIndex = 0;
+    var interval = 4000;
+  
+    function changeImage() {
+  
+      for (var i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+      }
+  
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].style.display = 'block';
     }
-
-    setInterval(changeImage,interval)
-}
-)
+  
+    setInterval(changeImage, interval)
+  }
+  )
+  
+  
+  function toggleMenu() {
+    var menu = document.getElementById("navList");
+    if (navList.style.display == "block") {
+      navList.style.display = "none";
+    }
+    else {
+      navList.style.display = "block";
+    }
+  }
